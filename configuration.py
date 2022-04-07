57dcd00e63569a526c38264af67b53264fbad597
unknown <shaked.hayek@mail.huji.ac.il>
2022-04-07 11:40:18 +0300

    Added category

--- configuration.py
+++ configuration.py
@@ -3,11 +3,13 @@
 DATA_FILE = "{user_id}_data.csv"
 COL_START = "start"
 COL_END = "end"
+COL_CATEGORY = "category"
 COL_ACTIVITY = "activity"
 COL_DURATION = "duration"
 DATA_FILE_COL = [
     COL_START,
     COL_END,
+    COL_CATEGORY,
     COL_ACTIVITY,
     COL_DURATION,
 ]
